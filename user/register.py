1851e6dbd2b878d86f81d13233bfc9427148cb6d
Valentin Lorentz <progval@gmail.com>
2010-12-12 16:38:03 +0100

    Add registration

--- user/register.py
+++ user/register.py
@@ -25,3 +25,99 @@
 # ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+import re
+import cgi
+import hashlib
+from common import db
+from common import html
+from common import exceptions
+
+testName = re.compile('^[a-zA-Z0-9_-]{2,36}$')
+testEmail = re.compile('^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_.-]{2,}\.[a-z]{2,}$')
+
+def run(environ):
+    status = '200 OK'
+    headers = []
+    responseBody = html.getHead(title='Créer un compte')
+    path = environ['module_path']
+    if path == '':
+        responseBody += """
+        <form action="submit.htm" method="POST">
+            <table>
+                <tr>
+                    <td><label for="name">Nom :</label></td>
+                    <td><input type="text" id="name" name="name" /></td>
+                </tr>
+                <tr>
+                    <td><label for="passwd1">Mot de passe :</label></td>
+                    <td>
+                        <input type="password" id="passwd1" name="passwd1" />
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="passwd2">
+                        Mot de passe (confirmation) :
+                    </label></td>
+                    <td>
+                        <input type="password" id="passwd2" name="passwd2" />
+                    </td>
+                </tr>
+                <tr>
+                    <td><label for="email">Adresse de courriel :</label></td>
+                    <td><input type="text" id="email" name="email" /></td>
+                </tr>
+                <tr>
+                    <td colspan="2">
+                        <input type="submit" value="S'inscrire" />
+                    </td>
+                </tr>
+            </table>
+        </form>"""
+    elif path == 'submit.htm':
+        assert environ['REQUEST_METHOD'].upper() == 'POST'
+        postData = cgi.FieldStorage(
+                fp=environ['wsgi.input'],
+                environ=environ,
+                keep_blank_values=True)
+        assert all((key in postData) for key in
+                   ('name', 'passwd1', 'passwd2', 'email'))
+        data = {}
+        for key in postData.keys():
+            data.update({key: str(postData[key].value)})
+        cursor = db.conn.cursor()
+        cursor.execute("SELECT name FROM users WHERE name=?",
+                       (data['name'],))
+        row = cursor.fetchone()
+        anyError = False
+        if row is not None:
+            responseBody += """<p>Il y a déjà un utilisateur ayant ce nom.
+                               Veuillez en choisir un autre.</p>"""
+            anyError = True
+        if data['passwd1'] != data['passwd2']:
+            responseBody += """<p>Le mot de passe et sa confirmation ne sont
+                               pas identiques.</p>"""
+            anyError = True
+        if not testName.match(data['name']):
+            responseBody += """<p>Le nom d'utilisateur est incorrect.
+                               Taille : de 2 à 36, et ne peux contenir que
+                               des caractères alphanumériques, des
+                               underscores et des tirets.</p>"""
+            anyError = True
+        if not testEmail.match(data['email']):
+            responseBody += """<p>L'adresse de courriel est invalide.</p>"""
+            anyError = True
+
+        if not anyError:
+            ##DB#users
+            cursor.execute("INSERT INTO users VALUES (?,?,?)", (
+                            data['name'],
+                            str(hashlib.sha1(data['passwd1'])),
+                            data['email']))
+            db.conn.commit()
+            responseBody += """Votre compte a été créé."""
+    else:
+        raise exceptions.Error404()
+
+    responseBody += html.getFoot()
+    return status, headers, responseBody
